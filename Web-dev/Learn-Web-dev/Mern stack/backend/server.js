43086f202f08a5551f772d0ceaa8cc7ff945d37a
Amine-bc <lm_bouchoucha@esi.dz>
2023-06-28 23:08:03 +0100

    web backend

--- Web-dev/Learn-Web-dev/Mern stack/backend/server.js
+++ Web-dev/Learn-Web-dev/Mern stack/backend/server.js
@@ -1,16 +1,22 @@
 const express = require("express");
-
+require("dotenv").config();
+const workoutRoutes = require('./routes/workout')
 // express app
 const app = express();
 
+// middleware
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.json({"Welcome": "to the home page"});
-    }
-);
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
 
 //listen for a port
-app.listen(3000, () => {
-    console.log("listening on port 3000!!");
+app.listen(process.env.PORT, () => {
+    console.log("listening on port",process.env.PORT);
     }
 );
